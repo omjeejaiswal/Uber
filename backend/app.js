3db7d32b02bc4ba4204c5f25a2dd6024039d38e4
omjeejaiswal <sureshjaiswal808@gmail.com>
2025-01-28 23:54:42 +0530

    connected to DB.

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,21 @@
 const dotenv = require('dotenv');
 dotenv.config();
-const express = require('express')
+const express = require('express');
 const cors = require('cors');
-const app = express();
 const connectToDb = require('./db/db');
 
+// Initialize the app
+const app = express();
+
+// Connect to the database
 connectToDb();
 
+// Middleware
 app.use(cors());
 
-
+// Routes
 app.get('/', (req, res) => {
     res.send('Hello World');
-})
+});
 
 module.exports = app;
-
--- backend/db/db.js
+++ backend/db/db.js
@@ -1,12 +1,16 @@
 const mongoose = require('mongoose');
 
 function connectToDb() {
-    mongoose.connect(process.env.DB_CONNECT)
+    mongoose.connect(process.env.DB_CONNECT, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
         .then(() => {
-            console.log('Connected to DB')
+            console.log('Connected to DB');
         })
-
-    .catch(err => console.log(err));
+        .catch((err) => {
+            console.log('Error connecting to DB:', err);
+        });
 }
 
 module.exports = connectToDb;
